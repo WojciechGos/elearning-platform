61c368467abccb9b9e910fe3c41f87448f3e9225
Wojtek <wojtekgg93@gmail.com>
2024-03-22 21:01:33 +0100

    add: course

--- backend/src/main/java/project/backend/courses/course/CourseService.java
+++ backend/src/main/java/project/backend/courses/course/CourseService.java
@@ -1,31 +1,37 @@
 package project.backend.courses.course;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.backend.exception.ResourceNotFoundException;
 
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
-@RestController
-@RequestMapping(path = "api/v1/courses")
 public class CourseService {
 
+    private final CourseRepository courseRepository;
+
+    public Course getCourseById(Long courseId) {
+        Course course = courseRepository.findById(courseId).orElseThrow(()-> new ResourceNotFoundException("Course not found with id [%s] ".formatted(courseId)));
+        return course;
+    }
+
+
     public List<Course> getCourses() {
-        return null;
+        return courseRepository.findAll();
     }
 
     public Course createCourse(Course course) {
-        return null;
+        return courseRepository.save(course);
     }
 
     public Course updateCourse(Course course) {
         return null;
     }
 
-    public Course getCourse(Long courseId) {
-        return null;
-    }
 
     public void deleteCourse(Long courseId) {
     }
