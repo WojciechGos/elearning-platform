672209f404faba6923b2693d33a2a48d171d6e78
Wojtek <wojtekgg93@gmail.com>
2024-03-23 15:34:07 +0100

    add: populate db

--- backend/src/main/java/project/backend/courses/category/Category.java
+++ backend/src/main/java/project/backend/courses/category/Category.java
@@ -22,4 +22,10 @@ public class Category {
     )
     private Long id;
     private String name;
+    private String description;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
--- backend/src/main/java/project/backend/courses/category/CategoryController.java
+++ backend/src/main/java/project/backend/courses/category/CategoryController.java
@@ -1,7 +1,11 @@
 package project.backend.courses.category;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +17,14 @@ import java.util.List;
 public class CategoryController {
     private final CategoryService categoryService;
 
+    @GetMapping
     public ResponseEntity<List<Category>> getCategories() {
-        return null;
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Category> getCategory(Long categoryId) {
-        return null;
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> getCategory(@PathVariable("categoryId") Long categoryId) {
+        return new ResponseEntity<>(categoryService.getCategory(categoryId), HttpStatus.OK);
     }
 
 }
