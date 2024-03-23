98c7a58cdf15b8da020b442f605cf99cd0bbbc51
Wojtek <wojtekgg93@gmail.com>
2024-03-23 15:40:31 +0100

    remove: unnecessary  imports

--- backend/src/main/java/project/backend/courses/category/CategoryController.java
+++ backend/src/main/java/project/backend/courses/category/CategoryController.java
@@ -2,7 +2,6 @@ package project.backend.courses.category;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
