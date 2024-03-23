b75d1a1c850f795000cb615c1b9b6e58ab405742
Wojtek <wojtekgg93@gmail.com>
2024-03-23 15:36:36 +0100

    add: cruds for lesson, categories update: updateCourse method

--- backend/src/main/java/project/backend/courses/course/CourseService.java
+++ backend/src/main/java/project/backend/courses/course/CourseService.java
@@ -1,7 +1,11 @@
 package project.backend.courses.course;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.backend.courses.lesson.Lesson;
+import project.backend.courses.lesson.LessonRequest;
+import project.backend.courses.lesson.LessonService;
 import project.backend.exception.ResourceNotFoundException;
 
 import java.util.List;
@@ -11,7 +15,7 @@ import java.util.List;
 public class CourseService {
 
     private final CourseRepository courseRepository;
-
+    private final LessonService lessonService;
     public Course getCourseById(Long courseId) {
         return courseRepository.findById(courseId).orElseThrow(()-> new ResourceNotFoundException("Course not found with id [%s] ".formatted(courseId)));
     }
@@ -25,12 +29,43 @@ public class CourseService {
         return courseRepository.save(course);
     }
 
-    public Course updateCourse(Course course) {
-        return null;
+    public Course updateCourse(Long id, Course course) {
+        Course updatedCourse = courseRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Course not found with id [%s] ".formatted(id)));
+
+        if(course.getTitle() != null) updatedCourse.setTitle(course.getTitle());
+        if(course.getDescription() != null) updatedCourse.setDescription(course.getDescription());
+        if(course.getPrice() != null) updatedCourse.setPrice(course.getPrice());
+        if(course.getLanguage() != null) updatedCourse.setLanguage(course.getLanguage());
+
+        return courseRepository.save(updatedCourse);
     }
 
 
     public void deleteCourse(Long courseId) {
+        /*
+            IRL it would be better to send request to administration to review deletion request of the course,
+            because some users may have already bought it.
+            However, for sake of simplicity we will just hide it
+         */
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new ResourceNotFoundException("Course not found with id [%s] ".formatted(courseId)));
+        course.setCourseState(CourseState.HIDDEN);
+        courseRepository.save(course);
     }
 
+    @Transactional
+    public Lesson addLessonToCourse(Long courseId, LessonRequest lesson) {
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new ResourceNotFoundException("Course not found with id [%s] ".formatted(courseId)));
+        Lesson createdLesson = lessonService.createLesson(lesson);
+        course.getLessons().add(createdLesson);
+        courseRepository.save(course);
+        return createdLesson;
+    }
+
+    public Course addCategoryToCourse(Long courseId, Long categoryId) {
+        return null;
+    }
+
+    public void removeCategoryFromCourse(Long courseId, Long categoryId) {
+
+    }
 }
