56e1c2e7637294e3cc5424bb9ca1e81a9ac638c6
Juliya246 <kurinajuliya246@gmail.com>
2024-03-24 14:49:14 +0100

    add: login and register page

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      //TODO WYSYANIE DANYCH DO SERWERA
+    }
+  }
+}
---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required], 
+    });
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      //TODO WYSYANIE DANYCH DO SERWERA
+    }
+  }
+}
