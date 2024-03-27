83772040bfadbb973865678207cd3bbe67282c5e
Wojtek <wojtekgg93@gmail.com>
2024-03-27 15:22:41 +0100

    in progres: course details

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'course-details', component: CourseDetailsComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, 
 ];
