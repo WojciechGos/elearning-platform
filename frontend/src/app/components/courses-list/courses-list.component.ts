879206d41bf5535112fa1a3d7d438510d09bbe46
zuzannawozniak <zuzanna.wk@interia.pl>
2024-04-13 15:12:46 +0200

    change: components parameters

--- frontend/src/app/components/courses-list/courses-list.component.ts
+++ frontend/src/app/components/courses-list/courses-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/interfaces/course.interface';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-courses-list',
@@ -6,17 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent implements OnInit {
-  courses = [
-    { title: 'Python od podstaw', image: 'https://via.placeholder.com/150', author: 'John Doe', price:  39.99},
-    { title: 'GIT dla zaawansowanych', image: 'https://via.placeholder.com/150', author: 'Ann Smith', price:  49.99},
-    { title: 'JavaScript dla początkujących', image: 'https://via.placeholder.com/150', author: 'Emily Johnson', price: 29.99 },
-    { title: 'Algorytmy i struktury danych', image: 'https://via.placeholder.com/150', author: 'Michael Brown', price: 59.99 },
-    { title: 'Machine Learning w praktyce', image: 'https://via.placeholder.com/150', author: 'Sophia Lee', price: 79.99 }
-  ];
+  courses: Course[] = [];
 
-  constructor() { }
+  constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
+    this.courseService.getCourses().subscribe((courses) => {
+      console.log(courses)
+      this.courses = courses;
+    });
   }
 
 }
--- frontend/src/app/components/price/price.component.ts
+++ frontend/src/app/components/price/price.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-price',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./price.component.css']
 })
 export class PriceComponent implements OnInit {
+  @Input() price!: number;
 
   constructor() { }
 
