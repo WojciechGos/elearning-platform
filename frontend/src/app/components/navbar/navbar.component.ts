876981ad5ec0d5a285493a171cd56d5b9b59ae6e
zuzannawozniak <zuzanna.wk@interia.pl>
2024-04-13 18:02:55 +0200

    add: categories list

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/interfaces/category.interface';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,14 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isListVisible = false;
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
   }
 
   onUserAuthenticate(): void{
     
+  }
+
+  showList() {
+    this.isListVisible = true;
+    if(this.categories.length === 0) {
+      this.categoryService.getCategories().subscribe((categories) => {
+        console.log(categories)
+        this.categories = categories;
+      });
+    }
+  }
+
+  hideList() {
+    this.isListVisible = false;
   }
 
 }
