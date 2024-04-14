3dbc7dbe2bf635d24c534b9d4c239b2a2c0a8fe3
Juliya246 <kurinajuliya246@gmail.com>
2024-04-14 17:31:51 +0200

    add: form validate

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -8,29 +8,31 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
+  loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
+      rememberMe: [false] 
     });
   }
 
-  onLogin() {
+  ngOnInit(): void {}
+
+  onLogin(): void {
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-      this.authService.login(this.loginForm.value.email, this.loginForm.value.password)
-    .subscribe({
-      next: (user) => {
-        console.log(user);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: (user) => {
+          console.log('Login successful', user);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+        }
+      });
+    } else {
+      console.log('Form is not valid');
     }
   }
 }
