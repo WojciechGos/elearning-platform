ea1e940cef9b871b352a2c4c105d0fa6fbd081a4
Juliya246 <kurinajuliya246@gmail.com>
2024-06-15 21:50:15 +0200

    fix: advanced course certificate

--- backend/src/main/java/project/backend/courses/course/service/CourseServiceImpl.java
+++ backend/src/main/java/project/backend/courses/course/service/CourseServiceImpl.java
@@ -1,9 +1,8 @@
 package project.backend.courses.course.service;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -242,11 +241,45 @@ public class CourseServiceImpl implements CourseService {
             Document document = new Document();
             PdfWriter.getInstance(document, out);
             document.open();
-            document.add(new Paragraph("Certificate of Completion"));
-            document.add(new Paragraph("This is to certify that"));
-            document.add(new Paragraph(user.getFirstName() + " " + user.getLastName()));
-            document.add(new Paragraph("has successfully completed the course"));
-            document.add(new Paragraph(course.getTitle()));
+            Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 30, Font.BOLD);
+            Font subTitleFont = new Font(Font.FontFamily.TIMES_ROMAN, 20, Font.BOLD);
+            Font bodyFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.NORMAL);
+
+            Paragraph title = new Paragraph("Certificate of Completion", titleFont);
+            title.setAlignment(Element.ALIGN_CENTER);
+            title.setSpacingAfter(50);
+            document.add(title);
+
+            Paragraph subTitle = new Paragraph("This is to certify that", subTitleFont);
+            subTitle.setAlignment(Element.ALIGN_CENTER);
+            subTitle.setSpacingAfter(30);
+            document.add(subTitle);
+
+            Paragraph name = new Paragraph(user.getFirstName() + " " + user.getLastName(), subTitleFont);
+            name.setAlignment(Element.ALIGN_CENTER);
+            name.setSpacingAfter(30);
+            document.add(name);
+
+            Paragraph courseTitle = new Paragraph("has successfully completed the course", bodyFont);
+            courseTitle.setAlignment(Element.ALIGN_CENTER);
+            courseTitle.setSpacingAfter(20);
+            document.add(courseTitle);
+
+            Paragraph courseName = new Paragraph(course.getTitle(), bodyFont);
+            courseName.setAlignment(Element.ALIGN_CENTER);
+            courseName.setSpacingAfter(50);
+            document.add(courseName);
+
+            LineSeparator separator = new LineSeparator();
+            separator.setLineColor(BaseColor.BLACK);
+            separator.setPercentage(80);
+            separator.setAlignment(Element.ALIGN_CENTER);
+            document.add(separator);
+            
+            Paragraph footer = new Paragraph("Congratulations!", subTitleFont);
+            footer.setAlignment(Element.ALIGN_CENTER);
+            footer.setSpacingBefore(50);
+            document.add(footer);
             document.close();
         } catch (DocumentException e) {
             throw new RuntimeException("Error generating PDF", e);
