1aebe5a2cf6d622bb42fc3faa52687238ce00c92
wojciech <wojtekgg93@gmail.com>
2024-06-20 09:06:19 +0200

    fix: find course by keyword in navbar in course search page

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Category } from 'src/app/interfaces/category.interface';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { Router } from '@angular/router';
@@ -10,16 +10,14 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  isListVisible = false;
-  categories: Category[] = [];
   searchInput: string = '';
   isLoggedIn: boolean = false;
   userInitials: string = '';
-  isMenuVisible = false;
+
+  @Output() searchEvent = new EventEmitter<string>();
 
   constructor(
     private router: Router,
-    private categoryService: CategoryService,
     private authService: AuthService
   ) {}
 
@@ -33,31 +31,16 @@ export class NavbarComponent implements OnInit {
   }
 
   onSearchHandler(): void {
+    this.searchEvent.emit(this.searchInput);
     this.router.navigate(['/course-search'], {
       queryParams: { keyword: this.searchInput },
     });
   }
 
-  showList() {
-    this.isListVisible = true;
-    if (this.categories.length === 0) {
-      this.categoryService.getCategories().subscribe((categories) => {
-        this.categories = categories;
-      });
-    }
-  }
-
-  hideList() {
-    this.isListVisible = false;
-  }
 
   getUserInitials(firstName: string, lastName: string): string {
     if (!firstName || !lastName) return '';
     return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
-  }
-
-  toggleMenu() {
-    this.isMenuVisible = !this.isMenuVisible;
   }
 
 
